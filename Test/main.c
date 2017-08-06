a74a47d3bde046fb14d43927acc15f82bf4d2dff
atulk <aniketkumar4568@gmail.com>
2017-08-06 15:34:15 +0530

    find aaverage of two numbers

--- Test/main.c
+++ Test/main.c
@@ -5,9 +5,20 @@ int main()
 {
     printf("Hello world!\n");
     test();
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int result = findAverage(a, b);
+    printf("%d", result);
     return 0;
 }
 
+
+int findAverage(int num1, int num2) {
+
+    return ( num1 + num2) /2;
+}
 
 int test() {
     printf("testing git commit ");
